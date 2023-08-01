ab6dd07a2c6ec769c6d1bc9a35ec083dde44fd06
Gautam Sachdeva <56038800+gautam232019@users.noreply.github.com>
2023-08-01 11:50:58 +0530

    worked on code quality

--- backend/cloud.py
+++
@@ -1,7 +0,0 @@
-import jenkins
-jenkins_url = 'http://3.21.206.3:8080'
-username = 'manas'
-password = '11b6b3df5dba37ac687fcdfa48ecded336'
-
-server = jenkins.Jenkins(jenkins_url, username=username, password=password)
-print(server)
